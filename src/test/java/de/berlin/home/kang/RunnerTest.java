13d8db52531537c46dce95238652490871d002b4
xinhua <xinhua.gu@gmail.com>
2018-09-28 23:19:52 +0200

    test main function

--- src/test/java/de/berlin/home/kang/RunnerTest.java
+++ src/test/java/de/berlin/home/kang/RunnerTest.java
@@ -1,13 +1,46 @@
 package de.berlin.home.kang;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 public class RunnerTest {
 
+	/*
+	 * @see https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+	 */
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	@BeforeEach
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@AfterEach
+	public void restoreStreams() {
+	    System.setOut(originalOut);
+	    System.setErr(originalErr);
+	}
+
 	@Test
-	public void test() {
+	public void test1() {
+		Runner.main(new String[] {});
+		assertTrue(!outContent.toString().isEmpty());
+	}
+	
+	@Test
+	public void test2() {
 		assertEquals(3, Runner.getFirst3Processes().size());
 	}
 
